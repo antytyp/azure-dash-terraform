3fde020f14f6c16aab786c49f6fbc66a7ae12cca
pawel <pwdyrlaga@gmail.com>
2025-12-28 18:15:04 +0100

    add additional variables info

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,16 @@
 variable "resource_group_name" {
+  type    = string
   default = "rg-dash"
 }
 
 variable "location" {
-  default = "westeurope"
+  type        = string
+  default     = "westeurope"
+  description = "Location of the resource group."
 }
 
 variable "web_app_name" {
+  type        = string
   default     = "pdyrlagadashwebapp"
   description = "Globally unique app name"
 }
