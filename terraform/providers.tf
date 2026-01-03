f0a5a73ab6e640cff3db0db4cc39e75d07f4c36c
pawel <pwdyrlaga@gmail.com>
2026-01-03 16:23:49 +0100

    rename resources

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,8 +12,8 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "terraform-rg"
-    storage_account_name = "terraformsaxyz123"
+    resource_group_name  = "rg-terraform"
+    storage_account_name = "saterraformxyz123"
     container_name       = "terraformstate"
     key                  = "terraform.tfstate"
   }
